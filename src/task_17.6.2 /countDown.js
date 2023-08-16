5b3df32cddafc8ae43e9d43e7e1e08805ccdec31
ParamonovIvan <131868856+ParamonovIvan@users.noreply.github.com>
2023-08-16 13:08:16 +0400

    Update countDown.js

--- src/task_17.6.2 /countDown.js
+++ src/task_17.6.2 /countDown.js
@@ -1,6 +1,6 @@
 export function countDown(n) {
   if ((typeof n !== 'number' || Number.isNaN(n)) {
-    throw new Error('Invalid n parameter');
+    throw new Error('n is not a number');
   };
   for (let i = n; i > 0; i--) {
     console.log(i)
