d25689581fb061b830e4e6110824afafc28deeba
ParamonovIvan <131868856+ParamonovIvan@users.noreply.github.com>
2023-08-16 13:28:53 +0400

    Update countDown.js

--- src/task_17.6.2 /countDown.js
+++ src/task_17.6.2 /countDown.js
@@ -1,8 +1,7 @@
 export function countDown(n) {
   if ((typeof n !== 'number' || Number.isNaN(n)) {
     throw new Error('Entered not a number!');
-  };
-  if (n < 1) {
+  } else (n < 1) {
     throw new Error('Number must be positive!');
   };
   for (let i = n; i > 0; i--) {
