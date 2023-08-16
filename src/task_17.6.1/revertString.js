4a090fd82245d076156666596792b977cf0738cc
ParamonovIvan <131868856+ParamonovIvan@users.noreply.github.com>
2023-08-16 12:12:50 +0400

    Update revertString.js

--- src/task_17.6.1/revertString.js
+++ src/task_17.6.1/revertString.js
@@ -1,1 +1,12 @@
+export function revertString(str) {
+  if (typeof str !== 'string') {
+    throw new Error('Not a string entered!');
+  };
 
+  let revertedStr = '';
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    revertedStr += str[i];
+  };
+  return revertedStr;
+}
